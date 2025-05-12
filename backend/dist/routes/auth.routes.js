75ff8801f586d2376f0b6a85663bbf4228746e54
Francis Cejas <fnmcejas@gmail.com>
2025-05-13 02:48:34 +0800

    temp simplify auth

--- backend/dist/routes/auth.routes.js
+++ backend/dist/routes/auth.routes.js
@@ -3,9 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// In authRoutes.ts
 const express_1 = __importDefault(require("express"));
-const auth_controller_1 = require("../controllers/auth.controller");
+// import { login, signUp } from '../controllers/auth.controller'; // Comment this out
 const router = express_1.default.Router();
-router.post('/signup', auth_controller_1.signUp);
-router.post('/login', auth_controller_1.login);
+router.post('/signup', (req, res) => {
+    res.send('Signup route hit');
+});
+router.post('/login', (req, res) => {
+    res.send('Login route hit');
+});
 exports.default = router;
