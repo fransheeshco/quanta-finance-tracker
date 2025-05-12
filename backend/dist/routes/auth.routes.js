ece7c9ff06c8b85356dcb3d6d2c0c7abd34570e7
Francis Cejas <fnmcejas@gmail.com>
2025-05-13 02:59:52 +0800

    change

--- backend/dist/routes/auth.routes.js
+++ backend/dist/routes/auth.routes.js
@@ -4,8 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const auth_controller_1 = require("../controllers/auth.controller");
+// import { login, signUp } from '../controllers/auth.controller'; // Comment this out
 const router = express_1.default.Router();
-router.post('/signup', auth_controller_1.signUp);
-router.post('/login', auth_controller_1.login);
+router.post('/signup', (req, res) => {
+    res.send('Signup route hit');
+});
+router.post('/login', (req, res) => {
+    res.send('Login route hit');
+});
 exports.default = router;
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { login, signUp } from '../controllers/auth.controller';
+// import { login, signUp } from '../controllers/auth.controller'; // Comment this out
 
 const router = express.Router();
 
-router.post('/signup', signUp);
-router.post('/login', login);
+router.post('/signup', (req, res) => {
+  res.send('Signup route hit');
+});
+
+router.post('/login', (req, res) => {
+  res.send('Login route hit');
+});
 
 export default router;
