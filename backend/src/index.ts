5d8782d8548a49973e7daa2a5c7882ad3e7648e6
Francis Cejas <fnmcejas@gmail.com>
2025-05-13 02:27:03 +0800

    Reduced routes for debugging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,15 @@ import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes';
-import userRoutes from './routes/user.routes';
-import categoryRoutes from "./routes/category.routes";
-import budgetRoutes from "./routes/budget.routes";
-import transactionRoutes from "./routes/transactions.routes";
-import savingsRoutes from "./routes/savings.routes";
-import expensesRoutes from "./routes/expenses.routes";
-import accountRoutes from "./routes/account.routes";
-import incomeRoutes from "./routes/income.routes";
-import transferRoute from "./routes/transfer.routes";
+// import userRoutes from './routes/user.routes';
+// import categoryRoutes from "./routes/category.routes";
+// import budgetRoutes from "./routes/budget.routes";
+// import transactionRoutes from "./routes/transactions.routes";
+// import savingsRoutes from "./routes/savings.routes";
+// import expensesRoutes from "./routes/expenses.routes";
+// import accountRoutes from "./routes/account.routes";
+// import incomeRoutes from "./routes/income.routes";
+// import transferRoute from "./routes/transfer.routes";
 import path from 'path';
 
 dotenv.config();
@@ -24,15 +24,19 @@ app.use(express.json());
 
 // API Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/auth/user', userRoutes) ;
-app.use('/api/auth/category', categoryRoutes);
-app.use('/api/auth/budgets', budgetRoutes);
-app.use('/api/auth/transaction', transactionRoutes);
-app.use('/api/auth/savings', savingsRoutes);
-app.use('/api/auth/expenses', expensesRoutes);
-app.use('/api/auth/account', accountRoutes);
-app.use('/api/auth/income', incomeRoutes);
-app.use('/api/auth/transfer', transferRoute);
+// app.use('/api/auth/user', userRoutes) ;
+// app.use('/api/auth/category', categoryRoutes);
+// app.use('/api/auth/budgets', budgetRoutes);
+// app.use('/api/auth/transaction', transactionRoutes);
+// app.use('/api/auth/savings', savingsRoutes);
+// app.use('/api/auth/expenses', expensesRoutes);
+// app.use('/api/auth/account', accountRoutes);
+// app.use('/api/auth/income', incomeRoutes);
+// app.use('/api/auth/transfer', transferRoute);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Testing root route');
+});
 
 // Serve static files from your frontend build directory
 const frontendBuildPath = path.join(__dirname, '../../frontend/build'); // Adjust the path as needed
