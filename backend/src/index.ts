cd53a54abb099252a3050ed4eaeed9a1276fc2ba
Francis Cejas <fnmcejas@gmail.com>
2025-05-13 03:56:00 +0800

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,14 +23,14 @@ app.use(cors());
 app.use(express.json());
 
 // API Routes
-// app.use('/api', authRoutes); // Ensure this line is commented out
-//app.use('/api/user', userRoutes) ;
-//app.use('/api/category', categoryRoutes);
-//app.use('/api/budgets', budgetRoutes);
-//app.use('/api/transaction', transactionRoutes);
-//app.use('/api/savings', savingsRoutes);
-//app.use('/api/expenses', expensesRoutes);
-//app.use('/api/account', accountRoutes);
+app.use('/api', authRoutes); // Ensure this line is commented out
+app.use('/api/user', userRoutes) ;
+app.use('/api/category', categoryRoutes);
+app.use('/api/budgets', budgetRoutes);
+app.use('/api/transaction', transactionRoutes);
+app.use('/api/savings', savingsRoutes);
+app.use('/api/expenses', expensesRoutes);
+app.use('/api/account', accountRoutes);
 app.use('/api/income', incomeRoutes);
 app.use('/api/transfer', transferRoute);
 
